625cd9fd4ffd978c2eb54e22b223aa8cb6134b2c
ModyA7med <forzaahly968@gmail.com>
2025-04-13 02:03:45 +0200

    ahmet

---
+++ Back-End/PromoTex-master/PromoTex/Utility/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace PromoTex.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            using (var client = new SmtpClient(_config["Smtp:Host"], int.Parse(_config["Smtp:Port"])))
+            {
+                client.Credentials = new NetworkCredential(_config["Smtp:User"], _config["Smtp:Pass"]);
+                client.EnableSsl = true;
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_config["Smtp:From"]),
+                    Subject = subject,
+                    Body = message,
+                    IsBodyHtml = true 
+                };
+                mailMessage.To.Add(toEmail);
+
+                await client.SendMailAsync(mailMessage);
+            }
+        }
+
+    }
+
+}

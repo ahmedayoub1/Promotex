1cff7b4a3b42122ba5b41320345c766bf112d771
Ayoub <mahmedmohammed2003@gmail.com>
2025-05-03 19:31:31 +0300

    APIS

--- Back-End/PromoTex-master/PromoTex/Utility/StaticData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PromoTex.Utility
-{
-    public static class StaticData
-    {
-        public const string AdminRole = "Admin";
-        public const string CustomerRole = "User";
-        public const string SellerRole = "Seller";
-    }
-}
